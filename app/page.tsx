fd294408728af29ea50f5c5ccb83daef8dcad4bc
Glenn Gijsberts <glenngijsberts95@gmail.com>
2023-05-29 23:18:14 +0200

    Introduce links

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,25 @@ import { Header } from '~/components/Header'
 import styles from './page.module.css'
 import { Section } from '~/components/Section'
 
+const links = [
+  {
+    title: 'GitHub',
+    href: 'https://www.github.com/glenngijsberts',
+  },
+  {
+    title: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/glenn-gijsberts-0341a463/',
+  },
+  {
+    title: 'Stack Overflow',
+    href: 'https://stackoverflow.com/users/5503094/giesburts',
+  },
+  {
+    title: 'Twitter',
+    href: 'https://twitter.com/giesburts',
+  },
+]
+
 const Home = () => {
   return (
     <main className={styles.pageLayout}>
@@ -18,6 +37,14 @@ const Home = () => {
           <a href="https://www.ticketswap.com">TicketSwap</a> and helping out as homework support
           volunteer at <a href="https://www.hackyourfuture.net/">HackYourFuture</a>.
         </p>
+
+        <ul className={styles.links}>
+          {links.map(link => (
+            <li key={link.href}>
+              <a href={link.href}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </Section>
     </main>
   )
