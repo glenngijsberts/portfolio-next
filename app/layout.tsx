7f4513830d85788511deb43fbfbd4a9aa46e97c7
Glenn Gijsberts <glenngijsberts95@gmail.com>
2023-05-29 22:58:26 +0200

    Make adjustments to meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
+import { Metadata } from 'next'
 import '~/styles/global.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Glenn Gijsberts - Front-end developer',
   description:
     "Graduated as interaction designer, now working as front-end developer on modern and user-friendly websites and apps. Currently I'm working in the growth team of TicketSwap",
+  keywords: [
+    'Next.js',
+    'React',
+    'TypeScript',
+    'Front-end developer',
+    'Interaction Designer',
+    'Glenn Gijsberts',
+  ],
+  authors: [{ name: 'Glenn Gijsberts', url: 'https://glenngijsberts.vercel.app' }],
 }
 
 interface Props {
