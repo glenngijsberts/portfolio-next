f195231921536278cba9b3aa9ba994bc894516b6
Glenn Gijsberts <glenngijsberts95@gmail.com>
2023-05-29 22:52:05 +0200

    Introduce first version of the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { Header } from '~/components/Header'
+import styles from './page.module.css'
+import { Section } from '~/components/Section'
+
 const Home = () => {
   return (
-    <main>
-      <h1>Glenn Gijsberts</h1>
+    <main className={styles.pageLayout}>
+      <Header />
+
+      <Section title="Glenn Gijsberts">
+        <p>
+          Graduated as{' '}
+          <a href="https://www.thuas.com/programmes/bachelors/user-experience-design">
+            interaction designer
+          </a>
+          , now working as front-end developer on modern and user-friendly websites and apps.
+          Currently I&apos;m working in the growth team of{' '}
+          <a href="https://www.ticketswap.com">TicketSwap</a> and helping out as homework support
+          volunteer at <a href="https://www.hackyourfuture.net/">HackYourFuture</a>.
+        </p>
+      </Section>
     </main>
   )
 }
