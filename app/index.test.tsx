b83288e9073c9be2c32fc3e27b971303a7351368
Glenn Gijsberts <glenngijsberts95@gmail.com>
2023-05-30 21:05:36 +0200

    Mock the Apollo client
    The Apollo Client is a server component, so in order to make it work
    for the tests I'm mocking the Apollo Client.

--- app/index.test.tsx
+++ app/index.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 import Home from './page'
 
 describe('Home', () => {
-  it('renders without crashing', () => {
-    render(<Home />)
+  it('renders without crashing', async () => {
+    render(await Home())
 
     const heading = screen.getByRole('heading', {
       name: /Glenn Gijsberts/i,
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom/extend-expect'
+
+jest.mock('./lib/apolloClient', () => {
+  return {
+    __esModule: true,
+    getClient: () => ({
+      query: () => ({
+        data: {
+          website: {
+            title: 'Glenn Gijsberts',
+          },
+        },
+      }),
+    }),
+  }
+})
