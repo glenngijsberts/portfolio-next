81e625fa97a6ac39debed7c7211157c681d44f04
Glenn Gijsberts <glenngijsberts95@gmail.com>
2023-06-15 22:41:53 +0200

    Implement Contentful for about me page

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -3,47 +3,35 @@ import { Section } from '~/components/Section'
 import { Button } from '~/components/Button'
 import Link from 'next/link'
 import styles from './about.module.css'
+import { GetPageInfoDocument } from '~/graphql/getPageInfo'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { getClient } from '~/lib/apolloClient'
 
-const AboutMe = () => (
-  <>
-    <Header title="About me" />
+const AboutMe = async () => {
+  const { data } = await getClient().query({
+    query: GetPageInfoDocument,
+    variables: {
+      id: '5KoQznZaM11LaYYp2Y5IOc',
+    },
+  })
 
-    <Section>
-      <p>
-        I&apos;m 27 years old and originally from{' '}
-        <a href="https://en.wikipedia.org/wiki/The_Hague">The Hague</a>. I&apos;m now living in{' '}
-        <a href="https://en.wikipedia.org/wiki/Amsterdam">Amsterdam</a>, working at TicketSwap as
-        front-end developer. I&apos;ve started learning web development in 2012 during my studies.
-        After doing several internships, I&apos;ve started working as a front-end developer in 2016
-        at <a href="https://sqits.nl/">Sqits</a>.
-      </p>
+  return (
+    <>
+      <Header title={data?.website?.title || 'About me'} />
 
-      <p>
-        In 2020 I graduated as Interaction Designer at The Hague University. In the meantime
-        I&apos;ve kept working as front-end developer, gaining experience in various stacks and
-        doing multiple projects. I&apos;ve experience in modern technologies like{' '}
-        <a href="https://stackshare.io/glenngijsberts">
-          React, Vue, TypeScript, GraphQL, Contentful, and more
+      <Section>
+        {data?.website?.description && documentToReactComponents(data?.website?.description.json)}
+
+        <a href="/glenngijsberts-resume.pdf">
+          <Button>Download my resume</Button>
         </a>
-        .
-      </p>
+      </Section>
 
-      <p>
-        Currently I&apos;m working at <a href="https://www.ticketswap.com">TicketSwap</a> in the
-        growth team. Projects I did includes several integrations with third-party services like
-        Google, Spotify and Optimizely, but also building new product features and maintaing the
-        existing codebase.
-      </p>
-
-      <a href="/glenngijsberts-resume.pdf">
-        <Button>Download my resume</Button>
-      </a>
-    </Section>
-
-    <footer className={styles.footer}>
-      <Link href="/">&larr; Back to home</Link>
-    </footer>
-  </>
-)
+      <footer className={styles.footer}>
+        <Link href="/">&larr; Back to home</Link>
+      </footer>
+    </>
+  )
+}
 
 export default AboutMe
